65d01d38100f537aa46d523c092ec599fc4ec597
AndreyBLR <Andrey_Kukalev@live.com>
2018-04-19 18:14:25 +0300

    Updates.

---
+++ AspNetCoreApp/AspNetCoreApp/ViewComponents/GreeterViewComponent.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using AspNetCoreApp.Services.Greeter;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AspNetCoreApp.ViewComponents
+{
+    public class GreeterViewComponent:ViewComponent
+    {
+        private readonly IGreeter _greeter;
+
+        public GreeterViewComponent(IGreeter greeter)
+        {
+            _greeter = greeter;
+        }
+
+        public IViewComponentResult Invoke()
+        {
+            var model = _greeter.GetGreeting();
+            return View("Default", model);
+        }
+    }
+}
